25059cb794b5513247ec43772b500885b92e19e7
hiphipdora <faaay.adistiii@gmail.com>
2024-12-23 10:39:19 +0700

    menambahkan api model dengan artisan

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\GenreController;
 use App\Http\Controllers\MovieController;
-use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,6 +10,5 @@ Route::get('/', function () {
 
 Route::get('/movies', [MovieController::class, 'index']);
 Route::get('/genres', [GenreController::class, 'index']);
-Route::get('/books', [BookController::class, 'index']);
 
 ?>
