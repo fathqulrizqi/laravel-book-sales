16db1ff90b81352af662776bf476a5b84f8da54b
hiphipdora <faaay.adistiii@gmail.com>
2024-12-24 09:49:41 +0700

    migration fresh dan tambah author controller

--- app/Http/Controllers/Api/AuthorController.php
+++ app/Http/Controllers/Api/AuthorController.php
@@ -1,10 +1,17 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
+use App\Models\Author;
 use Illuminate\Http\Request;
 
 class AuthorController extends Controller
 {
-    //
+    public function index(){
+        $authors = Author::all();
+        // $books = DB::select('SELECT * FROM books'); 
+
+        return response()->json($authors);
+    }
 }
