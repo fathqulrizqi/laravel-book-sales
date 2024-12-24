a832f91651292f89d742a46b499bbd2f857d4ec1
hiphipdora <faaay.adistiii@gmail.com>
2024-12-24 19:46:08 +0700

    menambahkan migration tables

---
+++ database/migrations/2024_12_24_194253_create_shipments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shipments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->string('tracking_number');
+            $table->text('shipping_address');
+            $table->enum('status', ['pending', 'shipped', 'delivered']);
+            $table->unsignedBigInteger('staff_shipped_by')->nullable();
+            $table->timestamp('staff_shipped_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shipments');
+    }
+};
