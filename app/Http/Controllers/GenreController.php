1f3c24550e07b0a4b7c3340f9434bcbd8ebacff3
hiphipdora <faaay.adistiii@gmail.com>
2024-12-24 09:38:50 +0700

    menambhakan genre model controller

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -1,16 +1,10 @@
-<?php 
+<?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
-use App\Models\Genre;
 
-class GenreController extends Controller{
-    public function index(){
-        $genre = new Genre;
-        $genres = $genre->getAllGenres();
-
-        return response()->json($genres);
-    }
-};
-
-?>
+class GenreController extends Controller
+{
+    //
+}
