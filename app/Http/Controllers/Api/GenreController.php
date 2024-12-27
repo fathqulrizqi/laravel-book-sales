416f847614298410271cdd295539fc089a83ab4c
hiphipdora <faaay.adistiii@gmail.com>
2024-12-27 09:17:07 +0700

    genre controller validator, fix users migration

--- app/Http/Controllers/Api/GenreController.php
+++ app/Http/Controllers/Api/GenreController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Genre;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class GenreController extends Controller
 {
@@ -12,6 +13,25 @@ class GenreController extends Controller
         $genres = Genre::all();
         // $books = DB::select('SELECT * FROM books'); 
 
-        return response()->json($genres);
+        if($genres->isEmpty()){
+            return response()->json([
+                "status" => true,
+                "message" => "Resource data not found!",
+            ], 200); //response gagal
+        }
+
+        return response()->json([
+            "status" => true,
+            "message" => "Get All Resource",
+            "data" => $genres
+        ], 200); //response berhasil
     }
+
+        public function store(Request $request)
+        {
+            Validator::make($request->all(),[
+                "name" => "required string",
+                "description" => "required string"
+            ]);
+        }
 }
