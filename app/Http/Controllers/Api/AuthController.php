c6594bbd561cab77d3e0c8b5ae57bc08d082ab79
hiphipdora <faaay.adistiii@gmail.com>
2024-12-31 09:59:21 +0700

    login auth

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -21,6 +22,59 @@ class AuthController extends Controller
         }
 
         // Create user 
-        
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        // Return response json useris created 
+        if ($user){
+            return response()->json([
+                'success' => true,
+                'message' => 'User created succesfully',
+            ], 201);
+        }
+
+        // Return response if process failed
+        return response()->json([
+            'success' => false,
+            'message' => 'User creation failed'
+        ], 409);
+
     }
+
+
+    public function login(Request $request){
+        // setup validator
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string'
+        ]);
+    //    check validator
+        if ($validator->fails()){
+            return response()->json($validator->errors(), 422);
+        }
+
+        // get credentials from request
+        $credentials = $request->only('email', 'password');
+
+        if(!$token = auth()->guard('api')->attempt($credentials)){
+            return response()->json([
+                'success' => false,
+                'message' => 'Email atau Password Anda Salah!'
+            ], 401);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Login Succesfullly',
+            'data' => [
+                'success' => true,
+                'user' => auth()->guard('api')->user(),
+                'token' => $token
+            ]
+            ]);
+    }
+    
 }
