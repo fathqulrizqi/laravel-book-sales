cdbeb2ac1a00eb6d32075b6aa28870d5f2c96fce
hiphipdora <faaay.adistiii@gmail.com>
2024-12-31 09:29:02 +0700

    auth

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request){
+        // Setup validator
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|max:255|unique:users',
+            'password' => 'required|string|max:8'
+        ]);
+
+        // Check Validator
+        if ($validator->fails()){
+            return response()->json($validator->errors(), 422);
+        }
+
+        // Create user 
+        
+    }
 }
