fdc3bf480e96d4b8c1340ac1c465d109cf500f96
hiphipdora <faaay.adistiii@gmail.com>
2025-01-02 13:55:44 +0700

    feat: add CRUD Book

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -1,8 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Http\Resources\OrderResource;
+use App\Models\Book;
 use App\Models\Order;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
@@ -17,7 +19,7 @@ class OrderController extends Controller
     public function store(Request $request){
         // validator 
         $validator = Validator::make($request->all(), [
-            "book_id" => "required|exist:books,id",
+            "book_id" => "required|exists:books,id",
             "quantity" => "required|integer|min:1"
         ]);
 
@@ -29,19 +31,48 @@ class OrderController extends Controller
             ], 422); // validasi create error
         }
 
+        // ambil data user yang sedang login
+        $user = auth('api')->user();
+
+        // cek login user
+        if(!$user){
+            return response()->json([
+                'status'=> false,
+                'message' => "Silahkan login terlebih dahulu"
+            ], 401);
+        }
+
+        // ambil satu data buku 
+        $book = Book::find($request->book_id);
+
+        // cek stok barang
+        if($book->stock < $request->quantity) {
+            return response()->json([
+                "status" => false,
+                "message" => "stok barang tidak cukup"
+            ], 400);
+        }
+
         // buat order number unik
         $order_number = "ORD-" . strtoupper(uniqid());
 
+        // hitung total harga 
+        $totalAmount = $book->price * $request->quantity;
+
+        // kurangi buku 
+        $book->stock -= $request->quantity;
+        $book->save();
 
         // insert data
         $order = Order::create([
-            "order_number" => '',
-            "customer_id" => '',
+            "order_number" => $order_number,
+            "customer_id" => $user->id,
             "book_id" => $request->book_id,
-            "total_amount" => '',
-            "status" => ''
+            "total_amount" => $totalAmount,
+            "status" => 'pending'
         ]);
 
         // return response 
+        return new OrderResource(true, 'Order Created Successfully', $order);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\AuthorController;
 use App\Http\Controllers\Api\BookController;
 use App\Http\Controllers\Api\GenreController;
+use App\Http\Controllers\Api\OrderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,11 +21,13 @@ Route::middleware(['auth:api'])->group(function () {
         Route::apiResource('/genres', GenreController::class)->only(['store', 'update', 'destroy']);
         Route::apiResource('/authors', AuthorController::class)->only(['store', 'update', 'destroy']);
     });
+    Route::middleware(['role:customer'])->group(function () {
+        Route::apiResource('/orders', OrderController::class);
+    });
 });
 
 Route::apiResource('/books', BookController::class)->only(['index', 'show']);
 Route::apiResource('/genres', GenreController::class)->only(['index', 'show']);
 Route::apiResource('/authors', AuthorController::class)->only(['index', 'show']);
-
 
 
